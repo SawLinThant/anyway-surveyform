d6836b553766bd6cb5c8d8cfcb7f52a1fbad56f6
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-10-21 15:25:35 +0700

    changepath

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import createApolloClient from "./graphql/apolloclient.js";
+import createApolloClient from "./graphql/apolloClient.js";
 import { ApolloProvider } from "@apollo/client";
 
 const apolloClient = createApolloClient();
